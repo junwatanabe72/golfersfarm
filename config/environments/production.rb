691e792b75c0204eb59a887087a6c59e8e933b60
jun.watanabe <junwatanabe72@yahoo.co.jp>
2019-07-07 05:47:20 +0000

    API

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      authentication:       :plain,
+      user_name:            ENV['SENDGRID_USERNAME'],
+      password:             ENV['SENDGRID_PASSWORD'],
+      domain:               'golfersfarm.com',
+      enable_starttls_auto: true
+    }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -91,18 +103,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
-
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.golfersfarm.com" }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-      user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_PASSWORD'],
-      domain: "www.golfersfarm.com",
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      authentication: :plain,
-      enable_starttls_auto: true
-  }
