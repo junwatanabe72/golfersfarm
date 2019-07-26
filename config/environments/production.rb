3ad0b45de196459a97802c327a22927639cf0ef3
jun.watanabe <junwatanabe72@yahoo.co.jp>
2019-07-26 13:42:12 +0000

    pg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+    host = 'golfersfarm.com'
+    config.action_mailer.default_url_options = { host: host }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
