2cdf32d7562a0ff193fb338f9ecd365f711ca30d
jun.watanabe <junwatanabe72@yahoo.co.jp>
2019-08-24 09:17:28 +0000

    relationships.dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,10 +48,10 @@ class User < ApplicationRecord
     
     #フォロー機能
     
-    has_many :relationships
-    has_many :followings, through: :relationships, source: :follow
-    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-    has_many :followers, through: :reverses_of_relationship, source: :user
+    has_many :relationships ,dependent: :destroy
+    has_many :followings, through: :relationships, source: :follow ,dependent: :destroy
+    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id' ,dependent: :destroy
+    has_many :followers, through: :reverses_of_relationship, source: :user ,dependent: :destroy
     
     def follow(other_user)
       unless self == other_user
