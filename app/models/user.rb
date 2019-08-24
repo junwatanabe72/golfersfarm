9eee49f577d7b7705be457c437817b40afb02cbe
jun.watanabe <junwatanabe72@yahoo.co.jp>
2019-08-24 09:08:56 +0000

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
    
    #バリテーション
     
+    validates :password, length: {minimum: 8 } 
+    
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
